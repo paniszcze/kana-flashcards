9e888311eefa2c3df8a8c430f178538c47cd4489
danny <bahuko@gmail.com>
2022-02-28 22:31:05 +0100

    clean create react app template

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Kana Flashcards</h1>
+        <p>Learn Japanese kana with customisable flashcards deck</p>
+      </div>
+    )
+  }
+}
+
+export default App;
