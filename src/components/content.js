4faa5384e0d21e6d7c8b04f37380a0b5a5f63078
danny <bahuko@gmail.com>
2022-03-03 01:09:36 +0100

    generate deck from dictionaries and pass random entry to flashcard on flip

--- src/components/content.js
+++ src/components/content.js
@@ -4,18 +4,30 @@ import Flashcard from './flashcard.js';
 import Counter from './counter.js';
 import Assessment from './assessment.js';
 
+import { hiragana } from '../data/hiragana';
+import { katakana } from '../data/katakana';
+
 import '../styles/content.css';
 
 export default function Content() {
   const [flipped, setFlipped] = useState(false);
+  const [card, setCard] = useState({});
 
   const flipFlashcard = () => {
+    if (flipped) {
+      let randomCard = chooseRandomCard(deck);
+      setCard(randomCard);
+    }
     setFlipped(!flipped);
   };
 
-  const card = {
-    front: "か",
-    back: "ka"
+  const deck = [...hiragana, ...katakana].map(item => {
+    return {front: item.kana, back: item.romaji};
+  });
+
+  const chooseRandomCard = (deck) => {
+    let randomIndex = Math.floor(Math.random() * deck.length);
+    return deck[randomIndex];
   }
 
   return (
