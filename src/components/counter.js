d58713d9157809c54392ea19070a7bc4c003d9f8
danny <bahuko@gmail.com>
2022-03-03 14:21:55 +0100

    change count to be evaluated as a sum of answers instead of onclick increment

--- src/components/counter.js
+++ src/components/counter.js
@@ -4,31 +4,31 @@ import '../styles/counter.css';
 
 export default function Counter() {
   const [count, setCount] = useState(0);
-  const [colors, setColors] = useState([0, 0, 0]);
+  const [answers, setAnswers] = useState([0, 0, 0]);
 
-  console.log(count, colors);
+  const handleClick = (answer) => {
+    setAnswers (prevAnswers => {
+      let newAnswers = [...prevAnswers];
 
-  const handleClick = (color) => {
-    setCount (prevCount => {
-      return prevCount + 1;
-    });
-
-    setColors (prevColors => {
-      let newColors = [...prevColors];
-      switch (color) {
+      switch (answer) {
         case "red":
-          newColors[0] += 1;
+          newAnswers[0] += 1;
           break;
         case "yellow":
-          newColors[1] += 1;
+          newAnswers[1] += 1;
           break;
         case "green":
-          newColors[2] += 1;
+          newAnswers[2] += 1;
           break;
         default:
-          return prevColors;
+          return prevAnswers;
       }
-      return newColors;
+
+      setCount (prevCount => {
+        return newAnswers.reduce((sum, a) => sum + a, 0);
+      });
+
+      return newAnswers;
     });
   }
 
@@ -50,7 +50,7 @@ export default function Counter() {
         </div>
       </div>
       <div className="assessment">
-        <p>{colors[0]} {colors[1]} {colors[2]}</p>
+        <p>{answers[0]} {answers[1]} {answers[2]}</p>
         <button className="red" onClick={() => handleClick("red")}>Nope</button>
         <button className="yellow" onClick={() => handleClick("yellow")}>Kinda</button>
         <button className="green" onClick={() => handleClick("green")}>Yep</button>
