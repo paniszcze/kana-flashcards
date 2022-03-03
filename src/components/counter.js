2c853f7b6953a19527ff8c6c25d0be7756e536a2
danny <bahuko@gmail.com>
2022-03-03 11:47:06 +0100

    add static circular counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,14 +1,28 @@
-import '../styles/counter.css';
+import "../styles/counter.css";
 
 export default function Counter() {
   return (
     <>
-      <div className="counter">Counter:</div>
+      <div className="counter">
+        <div calssName="stats">
+          <div className="percent">
+            <svg>
+              <circle className="base-circle" cx="70" cy="70" r="70"></circle>
+              <circle className="red-circle" cx="70" cy="70" r="70"></circle>
+              <circle className="yellow-circle" cx="70" cy="70" r="70"></circle>
+              <circle className="green-circle" cx="70" cy="70" r="70"></circle>
+            </svg>
+            <div className="number">
+                90%
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="assessment">
         <button className="red">Nope</button>
         <button className="yellow">Kinda</button>
         <button className="green">Yep</button>
       </div>
     </>
-  )
-};
+  );
+}
