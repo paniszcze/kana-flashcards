3f54af047c468466b4b335731c275f0b4ba0e981
danny <bahuko@gmail.com>
2022-03-03 15:19:55 +0100

    add counter limit and update answer bars dynamically

--- src/components/counter.js
+++ src/components/counter.js
@@ -3,57 +3,68 @@ import { useState } from 'react';
 import '../styles/counter.css';
 
 export default function Counter() {
+  const [limit, setLimit] = useState(200);
   const [count, setCount] = useState(0);
   const [answers, setAnswers] = useState([0, 0, 0]);
 
-  const handleClick = (answer) => {
-    setAnswers (prevAnswers => {
-      let newAnswers = [...prevAnswers];
+  const handleAnswer = (answer) => {
+    if (count < limit) {
+      setAnswers (prevAnswers => {
+        let newAnswers = [...prevAnswers];
 
-      switch (answer) {
-        case "red":
-          newAnswers[0] += 1;
-          break;
-        case "yellow":
-          newAnswers[1] += 1;
-          break;
-        case "green":
-          newAnswers[2] += 1;
-          break;
-        default:
-          return prevAnswers;
-      }
+        switch (answer) {
+          case "red":
+            newAnswers[0] += 1;
+            break;
+          case "yellow":
+            newAnswers[1] += 1;
+            break;
+          case "green":
+            newAnswers[2] += 1;
+            break;
+          default:
+            return prevAnswers;
+        }
 
-      setCount (prevCount => {
-        return newAnswers.reduce((sum, a) => sum + a, 0);
+        setCount (() => {
+          return newAnswers.reduce((sum, a) => sum + a, 0);
+        });
+
+        return newAnswers;
       });
-
-      return newAnswers;
-    });
+    }
   }
 
   return (
     <>
-      <div className="counter">
-        <div className="stats">
-          <div className="count">
-            <svg>
-              <circle className="base-circle" cx="70" cy="70" r="70"></circle>
-              <circle className="red-circle" cx="70" cy="70" r="70"></circle>
-              <circle className="yellow-circle" cx="70" cy="70" r="70"></circle>
-              <circle className="green-circle" cx="70" cy="70" r="70"></circle>
-            </svg>
-            <div className="number">
-                {count}
-            </div>
-          </div>
+      <div className="count">
+        <svg>
+          <circle className="base-circle" cx="70" cy="70" r="70"></circle>
+          <circle className="red-circle" 
+            style={{strokeDashoffset: `${count ? 440 - Math.floor(answers[0] / limit * 440)
+              - Math.floor(answers[1] / limit * 440) - Math.floor(answers[2] / limit * 440) : 440}px`}}
+            cx="70" cy="70" r="70">
+          </circle>
+          <circle className="yellow-circle" 
+            style={{strokeDashoffset: `${count ? 440 - Math.floor(answers[1] / limit * 440) 
+              - Math.floor(answers[2] / limit * 440) : 440}px`}}
+            cx="70" cy="70" r="70">
+          </circle>
+          <circle
+            className="green-circle"
+            style={{strokeDashoffset: `${count ? 440 - Math.floor(answers[2] / limit * 440) : 440}px`}}
+            cx="70" cy="70" r="70">
+          </circle>
+        </svg>
+        <div className="number">
+            {count}
         </div>
       </div>
       <div className="assessment">
         <p>{answers[0]} {answers[1]} {answers[2]}</p>
-        <button className="red" onClick={() => handleClick("red")}>Nope</button>
-        <button className="yellow" onClick={() => handleClick("yellow")}>Kinda</button>
-        <button className="green" onClick={() => handleClick("green")}>Yep</button>
+        <button className="red" onClick={() => handleAnswer("red")}>Nope</button>
+        <button className="yellow" onClick={() => handleAnswer("yellow")}>Kinda</button>
+        <button className="green" onClick={() => handleAnswer("green")}>Yep</button>
       </div>
     </>
   );
