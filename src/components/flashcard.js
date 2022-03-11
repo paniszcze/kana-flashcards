e63df1ea30192f37ed0374139f47622add33ec5a
danny <bahuko@gmail.com>
2022-03-11 22:06:51 +0100

    adjust responsiveness for mobile devices

--- src/components/flashcard.js
+++ src/components/flashcard.js
@@ -2,11 +2,9 @@ import "../styles/flashcard.css";
 
 export default function Flashcard({ card, flipped, flipFlashcard }) {
   return (
-    <div className="Flashcard" onClick={flipFlashcard}>
-      <div className={`flipper${flipped ? " flipped" : ""}`}>
-        <div className="front japanese">{card.front}</div>
-        <div className="back">{card.back}</div>
-      </div>
+    <div className={`Flashcard${flipped ? " flipped" : ""}`} onClick={flipFlashcard}>
+      <div className="front japanese">{card.front}</div>
+      <div className="back">{card.back}</div>
     </div>
   );
 }
